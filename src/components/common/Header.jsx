722646b5ed84169f9583dd8eb890183e628f38d7
wndud <ju03209@naver.com>
2026-04-10 16:35:57 +0900

    feat: Header 컴포넌트 nav에 호버 기능 추가(#24)

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,6 +4,8 @@ import { Settings } from 'lucide-react';
 export default function Header() {
   const user = { role: 'student' };
   const role = user?.role;
+  const linkStyle =
+    'px-2 py-2 cursor-pointer hover:bg-base-300/40 rounded-full ';
 
   return (
     <div className="w-full flex justify-between py-6 px-4">
@@ -14,10 +16,10 @@ export default function Header() {
         {role === 'student' && (
           <>
             <Link to="/my-courses">
-              <h4>수강 목록</h4>
+              <h4 className={linkStyle}>수강 목록</h4>
             </Link>
             <Link to="/mistake-notes">
-              <h4>오답 노트</h4>
+              <h4 className={linkStyle}>오답 노트</h4>
             </Link>
           </>
         )}
@@ -25,18 +27,17 @@ export default function Header() {
         {role === 'instructor' && (
           <>
             <Link to="/create-course">
-              <h4>강의 생성</h4>
+              <h4 className={linkStyle}>강의 생성</h4>
             </Link>
             <Link to="/my-courses">
-              <h4>강의 목록</h4>
+              <h4 className={linkStyle}>강의 목록</h4>
             </Link>
             <Link to="/quiz-analytics">
-              <h4>퀴즈 통계</h4>
+              <h4 className={linkStyle}>퀴즈 통계</h4>
             </Link>
           </>
         )}
-        <button>
-          {' '}
+        <button className={linkStyle}>
           {/* 추후 설정창 추가 */}
           <Settings size={22} />
         </button>
