eca3fae12de39641569cfd573bb00a8d0451d31b
kimgahyoun <hyeon3821@gmail.com>
2026-04-10 16:44:37 +0900

    feat: CourseCatalogPage lectures 목 데이터 생성 (#16)

--- src/pages/CourseCatalogPage.jsx
+++ src/pages/CourseCatalogPage.jsx
@@ -1,8 +1,8 @@
 import LectureList from '../components/common/course/LectureList';
-import { DUMMY_COURSES } from '../data/courses';
+import { DUMMY_LECTURES } from '../data/lectures';
 
 const CourseCatalogPage = () => {
-  return <LectureList lectures={DUMMY_COURSES} />;
+  return <LectureList lectures={DUMMY_LECTURES} />;
 };
 
 export default CourseCatalogPage;
