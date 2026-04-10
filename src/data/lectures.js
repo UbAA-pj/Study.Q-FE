0a68ab0b1bdfb24b2373617c71011eafa24ad8e2
kimgahyoun <hyeon3821@gmail.com>
2026-04-10 16:38:28 +0900

    feat: 강의 목 데이터 추가 (#16)

---
+++ src/data/lectures.js
@@ -0,0 +1,11 @@
+export const DUMMY_LECTURES = [
+  { id: 1, image: 'https://picsum.photos/seed/11/300/300', title: '1강. HTML이란?', date: 'December 29, 2012' },
+  { id: 2, image: 'https://picsum.photos/seed/12/300/300', title: '2강. CSS 기초', date: 'November 28, 2015' },
+  { id: 3, image: 'https://picsum.photos/seed/13/300/300', title: '3강. flex 박스 기초', date: 'August 2, 2013' },
+  { id: 4, image: 'https://picsum.photos/seed/14/300/300', title: '4강. JavaScript 기초', date: 'August 24, 2013' },
+  { id: 5, image: 'https://picsum.photos/seed/15/300/300', title: '5강. JavaScript 중급', date: 'December 2, 2018' },
+  { id: 6, image: 'https://picsum.photos/seed/16/300/300', title: '6강. JavaScript 상급', date: 'November 7, 2017' },
+  { id: 7, image: 'https://picsum.photos/seed/17/300/300', title: '7강. 간단한 계산기 만들기 1', date: 'July 14, 2015' },
+  { id: 8, image: 'https://picsum.photos/seed/18/300/300', title: '8강. 간단한 계산기 만들기 2', date: 'May 29, 2017' },
+  { id: 9, image: 'https://picsum.photos/seed/19/300/300', title: '9강. 미니 프로젝트', date: 'October 31, 2017' },
+];
