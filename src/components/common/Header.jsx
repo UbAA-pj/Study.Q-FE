df90dc7f8c5d439b3d433b93488228581192eb71
wndud <ju03209@naver.com>
2026-04-10 14:10:39 +0900

    feat: Header 컴포넌트 구현(#19)

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import { Settings } from 'lucide-react';
+
+export default function Header() {
+  const user = { role: 'student' };
+  const role = user?.role;
+
+  return (
+    <div className="w-full flex justify-between py-6 px-4">
+      <Link to="/">
+        <h1 className="text-logo">Study.Q</h1>
+      </Link>
+      <nav className="flex items-center gap-2">
+        {role === 'student' && (
+          <>
+            <Link to="/my-courses">
+              <h4>수강 목록</h4>
+            </Link>
+            <Link to="/mistake-notes">
+              <h4>오답 노트</h4>
+            </Link>
+          </>
+        )}
+
+        {role === 'instructor' && (
+          <>
+            <Link to="/create-course">
+              <h4>강의 생성</h4>
+            </Link>
+            <Link to="/my-courses">
+              <h4>강의 목록</h4>
+            </Link>
+            <Link to="/quiz-analytics">
+              <h4>퀴즈 통계</h4>
+            </Link>
+          </>
+        )}
+        <button>
+          {' '}
+          {/* 추후 설정창 추가 */}
+          <Settings size={22} />
+        </button>
+      </nav>
+    </div>
+  );
+}
