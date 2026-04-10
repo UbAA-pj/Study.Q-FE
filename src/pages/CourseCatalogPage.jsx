9b44c68c122be20c568d715f9e93a26803157de2
kimgahyoun <hyeon3821@gmail.com>
2026-04-10 15:12:26 +0900

    feat: 강의 리스트 구현 (#16)

--- src/pages/CourseCatalogPage.jsx
+++ src/pages/CourseCatalogPage.jsx
@@ -1,5 +1,8 @@
+import LectureList from '../components/LectureList';
+import { DUMMY_COURSES } from '../data/courses';
+
 const CourseCatalogPage = () => {
-  return <div>Course Catalog</div>;
+  return <LectureList lectures={DUMMY_COURSES} />;
 };
 
 export default CourseCatalogPage;
