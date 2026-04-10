891a7c6c8390765b41dba1e65ec306ce70904fcc
박주영 <162546405+wnduddlekd@users.noreply.github.com>
2026-04-10 14:24:29 +0900

    [FEAT] 공통 컴포넌트 Header 구현 (#20)
    * chore: lucide-react 아이콘 라이브러리 추가(#19)
    
    * feat: Header 컴포넌트 구현(#19)

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Breadcrumb from '../components/Breadcrumb';
+import Header from '../components/common/Header';
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col items-center">
       <div className="w-full max-w-250 flex flex-col flex-1">
         {/* Header */}
-
+        <Header />
         <Breadcrumb />
 
         {/* Page */}
