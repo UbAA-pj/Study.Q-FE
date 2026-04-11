d9b56685d6c7e5ed6a20e253ba96f3701d6a8110
kimgahyoun <hyeon3821@gmail.com>
2026-04-11 16:47:13 +0900

    feat: CourseDetailPage에 NextLectureList 연결 (#17)

--- src/pages/CourseDetailPage.jsx
+++ src/pages/CourseDetailPage.jsx
@@ -1,5 +1,12 @@
+import NextLectureList from '../components/common/course/NextLectureList';
+import { DUMMY_LECTURES } from '../data/lectures';
+
 const CourseDetailPage = () => {
-  return <div>Course Detail</div>;
+  return (
+    <div className="flex justify-end px-9 py-6">
+      <NextLectureList lectures={DUMMY_LECTURES} activeId={DUMMY_LECTURES[0]?.id} />
+    </div>
+  );
 };
 
 export default CourseDetailPage;
