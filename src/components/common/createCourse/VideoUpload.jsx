2138e76175ea1c7c31ca696cf1d997f655f218fe
wnduddlekd <ju03209@naveer.com>
2026-04-12 19:01:22 +0900

    feat: 비디오 미리보기 상위 파일로 전달(#38)

--- src/components/common/createCourse/VideoUpload.jsx
+++ src/components/common/createCourse/VideoUpload.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import Button from '../Button';
 import { Video } from 'lucide-react';
 
-export default function VideoUpload() {
+export default function VideoUpload({ setVideo, onUploadComplete }) {
   const fileRef = useRef(null);
-  const [video, setVideo] = useState(null);
 
   const handleClick = () => {
     fileRef.current.click();
@@ -12,25 +11,18 @@ export default function VideoUpload() {
 
   const handleChange = (e) => {
     const file = e.target.files[0];
-    if (file) {
-      setVideo(file);
-    }
+    if (!file) return;
+
+    setVideo(file);
+    onUploadComplete?.();
   };
 
-  useEffect(() => {
-    return () => {
-      if (video) URL.revokeObjectURL(video);
-    };
-  }, [video]);
-
   return (
-    <div className="flex flex-col gap-3">
-      {/* 버튼 */}
+    <>
       <Button icon={<Video />} onClick={handleClick}>
         동영상 업로드
       </Button>
 
-      {/* 숨겨진 input */}
       <input
         type="file"
         accept="video/*"
@@ -38,15 +30,6 @@ export default function VideoUpload() {
         className="hidden"
         onChange={handleChange}
       />
-
-      {/* 미리보기 */}
-      {video && (
-        <video
-          controls
-          className="w-full max-w-md rounded-md"
-          src={URL.createObjectURL(video)}
-        />
-      )}
-    </div>
+    </>
   );
 }
